4211643a110a3c405885f6b8c2eeffe8c2ef0e1c
houdunfuwenbin <1964855183@qq.com>
2019-11-29 14:51:00 +0800

    注册验证码

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,27 @@
 <?php
+
+// 在之前的路由后面配上中间件
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+// 在之前的路由里加上一个 verify 参数
+//Auth::routes(['verify' => true]);
+
+// 用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
