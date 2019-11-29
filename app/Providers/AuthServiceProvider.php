f135022223fe9b0f792c29f1b530ab8f94c5d619
houdunfuwenbin <1964855183@qq.com>
2019-11-29 16:19:49 +0800

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,12 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        //在 AuthServiceProvider 注册这个授权策略，从 Laravel 5.8 起，我们可以定义一个回调函数来让 Laravel 自己去寻找模型所对应的授权策略文件
+        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件的寻找逻辑
+        Gate::guessPolicyNamesUsing(function ($class) {
+            // class_basename 是 Laravel 提供的一个辅助函数，可以获取类的简短名称
+            // 例如传入 \App\Models\User 会返回 User
+            return '\\App\\Policies\\'.class_basename($class).'Policy';
+        });
     }
 }
